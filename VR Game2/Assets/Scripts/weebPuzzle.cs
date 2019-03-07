e510b0542e2474885cbb105ae155a8d67675c8a3
tabacchit <tabacchi0128@yahoo.com>
2019-03-07 16:58:50 -0500

    done weeb shit puzzle

---
+++ VR Game2/Assets/Scripts/weebPuzzle.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weebPuzzle : MonoBehaviour
+{
+
+   public bool hasYellow = false;
+    public bool hasRed = false;
+    public bool hasGreen = false;
+    public bool hasBlue = false;
+
+    public bool poster = false;
+
+    public bool hasKey = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(hasYellow==true && hasRed==true&&hasBlue==true&&hasGreen==true&&poster==true)
+        {
+            GameObject.Find("Laptop_Off").active = false;
+            hasYellow = false;
+        }
+    }
+
+    public void hasyellow()
+    {
+        hasYellow = true;
+    }
+
+    public void hasposter()
+    {
+        poster = true;
+    }
+
+    public void hasred()
+    {
+        hasRed = true;
+    }
+
+    public void hasgreen()
+    {
+        hasGreen = true;
+    }
+
+    public void hasblue()
+    {
+        hasBlue= true;
+    }
+
+    public void haskey()
+    {
+        hasKey = true;
+    }
+
+
+}
