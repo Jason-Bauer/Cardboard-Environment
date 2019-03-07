4ccb90a1242473a580caf8601f8cc54685bdaae3
Parker Wilson <pfw4498@g.rit.edu>
2019-03-07 15:19:33 -0500

    Western room cleaned up

--- VR Game2/Assets/YeeHawCowboy/OpenChest.cs
+++ VR Game2/Assets/YeeHawCowboy/OpenChest.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class OpenChest : MonoBehaviour
 {
+    public GameObject manager;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +20,9 @@ public class OpenChest : MonoBehaviour
 
     public void ChestOpen()
     {
-        GetComponent<Animator>().Play("TreasureChest_OPEN", 0,0.0f);
+        if (manager.GetComponent<PuzzleManager>().hasBullets)
+        {
+            GetComponent<Animator>().Play("TreasureChest_OPEN", 0, 0.0f);
+        }
     }
 }
